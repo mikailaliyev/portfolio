193a40f7ebfa78df766b532ed0b0f9f42169b37e
mikailaliyev <mikail.aliyev@gmail.com>
2023-05-28 00:03:15 +0400

    added: about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function About() {
+  return (
+    <>
+      <title>About</title>
+      <h1>About me</h1>
+      <Link href={"/"} >Back home</Link>
+    </>
+  );
+}
+export default About;
