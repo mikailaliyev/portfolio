9701c97bea1078e4895a5f696b228ae50d48c81a
mikailaliyev <mikail.aliyev@gmail.com>
2023-05-29 16:10:19 +0400

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link";
-
-function About() {
+async function About() {
+  const data = await fetch(
+    "https://test-fastapi-app.vercel.app/88"
+  );
+  const pageName= await data.json();
+  console.log(pageName);
   return (
     <>
       <title>About</title>
       <h1>About me</h1>
+      <h2>{pageName}</h2>
     </>
   );
 }
