d361687d3cb3100da08c30e79c26240198dbe977
mikailaliyev <mikail.aliyev@gmail.com>
2023-05-31 22:20:42 +0400

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercelEdge from "@astrojs/vercel/edge";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercelEdge(),
+});
